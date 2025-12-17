2b45c9f5a979ad21c1ceab00d93fd8b94b979739
harsh sareen <harshsareen03@gmail.com>
2025-12-17 11:35:04 +0530

    'unsup_neural'

--- ttest.py
+++ ttest.py
@@ -1,9 +1,13 @@
 from sklearn.model_selection import train_test_split
+
 import pandas as pd
 
 
 df=pd.read_csv('sp.csv')
 x=df[['Age','salary']]
+
+
+df=pd.read_csv('sp.csv')
 
 y=df['purchased']
 x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2, random_state=42)
